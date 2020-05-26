c19ba4c8e6c1c3b548bee5d156467c160353a9ed
Fernando Coca <61056638+fcocadatavenues@users.noreply.github.com>
2020-05-26 10:22:20 +0200

    Update Configuration.php
    fixed configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('liuggio_excel');
+        $treeBuilder = new TreeBuilder('liuggio_excel');
+        $treeBuilder->getRootNode();
 
         return $treeBuilder;
     }
